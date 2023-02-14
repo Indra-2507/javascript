2523ba5f92c7596a047dd8952bd7a7c90589570a
Indra Lima <indraalima@gmail.com>
2023-02-13 21:51:43 -0300

    clase1

---
+++ variables/js/scrpit.js
@@ -0,0 +1,16 @@
+let firstname= "Indra" //string- cadena de texto
+let lastname= "Lima"
+let birthDateYear=1988 //number- interger
+let PI=3.141518 //number -float
+let knowshowtodrive= true //boolean
+let willDinner
+let presentation= `Hola mi nombre es ${firstname} ${lastname} y nací en el año ${birthDateYear}`
+
+console.log(firstname)
+console.log(lastname)
+console.log(birthDateYear)
+console.log(firstname + " " + lastname)//para espaclogiar el contenido concatenando
+console.log(firstname, lastname) //llego al msmo resultado pero no estoy concatenando
+console.log("Hola, mi nombre es " + firstname + " " + lastname + "y naci en el año" + " " + birthDateYear)
+console.log(`Hola mi nombre es ${firstname} ${lastname} y nací en el año ${birthDateYear}`) //sugar syntax
+console.log(presentation)
